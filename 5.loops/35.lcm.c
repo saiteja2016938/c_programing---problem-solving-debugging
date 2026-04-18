d6f81b32afe76a8de450b9d4d817c1e380a3940e
saitejasagar <160340358+saiteja2016938@users.noreply.github.com>
2026-04-18 09:56:45 +0530

    Fix formatting and structure in LCM program

--- 5.loops/35.lcm.c
+++ 5.loops/35.lcm.c
@@ -1,7 +1,8 @@
 //  crate a program to find least common multiple of two numbers
 
 #include<stdio.h>
-int main(){
+int main()
+{
    int frist,second;
    printf("enter the frist number :");
    scanf("%d", &frist);
@@ -10,12 +11,13 @@ int main(){
 
    int min = frist < second ? frist : second;
    int max = frist * second;
-   for(int i = min ; i <= max; i++){
-    if(i % frist == 0 && i % second == 0 ){
+   for(int i = min ; i <= max; i++)
+   {
+    if(i % frist == 0 && i % second == 0 )
+    {
       printf("the LCM of %d and %d is %d",frist,second,i);
       break;
     }
    }
 return 0;
-
 }
